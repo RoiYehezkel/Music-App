fedb37f11c489764f077f18308fbe5e2afca8155
RoiYehezkel <roi5634@gmail.com>
2023-09-14 18:57:53 +0300

    deploy website with firebase

--- src/spotify.tsx
+++ src/spotify.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const scopes = ["user-library-read", "playlist-read-private"];
+const redirectUri = "https://music-app-a37eb.web.app";
 
 export const loginEndpoint = `${process.env.REACT_APP_AUTH_ENDPOINT}client_id=${
   process.env.REACT_APP_CLIENT_ID
-}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${scopes.join(
+}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
 
